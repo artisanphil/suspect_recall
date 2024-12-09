4e6b6668617cd6d32151405b64616429be02d72d
Philip Perry <philip.perry@learnclick.com>
2024-12-09 08:39:58 +0000

    added suspect nr 4 and scroll to top / bottom

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -24,6 +24,7 @@ const Game: React.FC = () => {
 
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     const fetchPerson = async () => {
       try {
         const response = await fetch('/api/person', { credentials: 'include' });
